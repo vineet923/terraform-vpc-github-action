3f955625f409d6f31c3e5f8e1ad54484f41433ca
vineetkumaryadav92@gmail.com <vineet@C40LAP-13069.india.rsystems.com>
2021-12-22 14:13:54 +0530

    terraform

---
+++ vars.tf
@@ -0,0 +1,16 @@
+variable "REGION" {
+  default = "ap-south-1"
+}
+
+variable "ZONE1" {
+  default = "ap-south-1a"
+}
+
+variable "ZONE2" {
+  default = "ap-south-1b"
+}
+
+variable "ZONE3" {
+  default = "ap-south-1c"
+}
+
